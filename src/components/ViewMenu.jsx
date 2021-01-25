334b7df8a85170006c24ed4565b3fd55a1cfd5c7
ClarissaPaitanS <clarypaitan@gmail.com>
2021-01-25 18:05:09 -0500

    boilerplate

---
+++ src/components/ViewMenu.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const ViewMenu = () => (
+  <div>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#">Logo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#"><Link to="/" className="btn-navbar nav-link">Home</Link></Nav.Link>
+          <Nav.Link href="#servicios"><Link to="/servicios" className="btn-navbar nav-link">Servicios</Link></Nav.Link>
+          <Nav.Link href="#prestamos"><Link to="/prestamos" className="btn-navbar nav-link">Préstamos</Link></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
+);
+
+export default ViewMenu;
