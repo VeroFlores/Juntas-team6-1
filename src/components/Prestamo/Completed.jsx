32f5bec53b941df10abca537ab21da102a4ec70e
ClarissaPaitanS <clarypaitan@gmail.com>
2021-01-27 17:10:12 -0500

    persistir datos

--- src/components/Prestamo/Completed.jsx
+++ src/components/Prestamo/Completed.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Image from 'react-bootstrap/Image';
 import { useHistory } from 'react-router-dom';
 
@@ -14,25 +15,32 @@ export function ButtonToSelect() {
     </button>
   );
 }
-const Completed = () => (
-  <>
-    <section id="prestamos" className="padded">
-      <Image src="https://susociodenegocios.com/wp-content/uploads/2019/02/mujer-duena-de-su-negocio.jpg" fluid />
-      <h1> Prestamos</h1>
-    </section>
-    <div className="container">
-      <ul className="progressBar">
-        <li className="active"> Coloca tus datos</li>
-        <li className="active"> Selecciona el banco/tasa</li>
-        <li className="active"> Proceso Exitoso</li>
-        <li> Final</li>
-      </ul>
-    </div>
-    <h1>Completado</h1>
-    <div>
-      <ButtonToSelect />
-    </div>
-  </>
-);
+const Completed = (props) => {
+  const { formData } = props;
+  console.log('Paso 3  - Traer datos del formulario Paso 1:', formData);
+  return (
+    <>
+      <section id="prestamos" className="padded">
+        <Image src="https://susociodenegocios.com/wp-content/uploads/2019/02/mujer-duena-de-su-negocio.jpg" fluid />
+        <h1> Prestamos</h1>
+      </section>
+      <div className="container">
+        <ul className="progressBar">
+          <li className="active"> Coloca tus datos</li>
+          <li className="active"> Selecciona el banco/tasa</li>
+          <li className="active"> Proceso Exitoso</li>
+          <li> Final</li>
+        </ul>
+      </div>
+      <h1>Completado</h1>
+      <div>
+        <ButtonToSelect />
+      </div>
+    </>
+  );
+};
 
 export default Completed;
+Completed.propTypes = {
+  formData: PropTypes.shape.isRequired,
+};
